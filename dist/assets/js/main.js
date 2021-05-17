e3d60876c5924063156da93b204cf6b14495d5f7
xdan <skoder@ya.ru>
2021-05-18 00:53:32 +0300

    New version 1.0.2. Read more https://github.com/xdan/docium/blob/master/CHANGELOG.MD

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,1 +1,5 @@
-`use strict`;
+/*!
+ * Docium (https://xdsoft.net/docium/)
+ * Released under MIT see LICENSE.txt in the project root for license information.
+ * Copyright (c) 2013-2021 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ */
