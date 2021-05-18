b2c6f67b70ffb9ecd109e7aa12dbbd438d5aa7c0
xdan <skoder@ya.ru>
2021-05-19 00:42:48 +0300

    Remove node_modules

---
+++ docs/assets/js/main.js
@@ -0,0 +1,5 @@
+/*!
+ * Docium (https://xdsoft.net/docium/)
+ * Released under MIT see LICENSE.txt in the project root for license information.
+ * Copyright (c) 2013-2021 Valeriy Chupurnov. All rights reserved. https://xdsoft.net
+ */
